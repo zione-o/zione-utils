d5dac77524051a654489783fd9ed615f9adfa3e8
zione <zione8848@gmail.com>
2025-12-18 14:29:21 +0330

    add sleep.js

--- src/sleep.js
+++ src/sleep.js
@@ -1,1 +1,10 @@
-
+/**
+ * Sleep utility
+ * Resolves a promise after given milliseconds.
+ *
+ * @param {number} ms
+ * @returns {Promise<void>}
+ */
+export function sleep(ms = 0) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
